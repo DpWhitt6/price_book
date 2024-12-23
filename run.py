a06bab235a27bc29016957d9f6af32d98d5db3ed
Daniel Whittingham <zy1pk0z7@students.codeinstitute.net>
2024-12-23 13:34:02 +0000

    completed back stops to ensure smooth transistion through

--- run.py
+++ run.py
@@ -12,16 +12,16 @@ def welcome():
     """
     while True:
         print(Style.BRIGHT + "Welcome to Price Book's Wall calculator \n")
-        print('We aim to provide you with market rates (Average rates)' +
-              'order quantities.\n')
-        print('Allowing you to spend more time building than quoting\n')
+        print('We aim to provide you with order quantities.\n')
+        print('Allowing you to spend more time building than ordering\n')
         print('Let us start bying giving your project a name' +
               '(1-50 characters) \n')
 
         input_name = input('Please enter your project name: \n')
         while (len(input_name) < 1 or len(input_name) > 50):
             print(Fore.RED + Style.BRIGHT + 'Sorry, you need to enter' +
-                  'a project name to proceed.')
+                  ' a project name to proceed with more than 1 and' 
+                  ' less than 50 characters.')
             input_name = input(Fore.BLACK +
                                'Please enter your project name: \n')
 
@@ -29,20 +29,6 @@ def welcome():
         print(f'Great you have named your Project {project_name}\n')
         break
     return True
-
-
-def wall_build_up():
-
-    print(Fore.BLACK + 'Now let us enter your wall dimensions\n')
-    wall_area, wall_length = wall_meter_square_calc()
-
-    print(Fore.GREEN + f'Great! you have {wall_area} m2\n')
-
-    get_stud_quantities(wall_length)
-
-    get_insulations_quantites(wall_area)
-
-    get_wall_linings_quantities(wall_area, wall_length)
 
 
 def wall_meter_square_calc():
@@ -55,6 +41,7 @@ def wall_meter_square_calc():
             print(Fore.RED + Style.BRIGHT + 'Please only enter numbers')
 
     area = wall_length * wall_height
+    print(Fore.GREEN + Style.BRIGHT + f'Great! you have {area} m2\n')
     return area, wall_length
 
 
@@ -64,14 +51,20 @@ def get_stud_quantities(wall_length):
 
     while True:
         try:
-            stud = input(Fore.BLACK + 'Choose a stud ref from the table above:'
-                         + '\n')
-            zero_index_stud = int(stud)-1
-            centers_answers = inquirer.prompt(centers)
-            cntrs = centers_answers['centers']
-            break
+            stud = int(input(Fore.BLACK + 'Choose a stud ref from the table above:'
+                         + '\n'))
+            if stud >= 1 and stud <= 7:
+                zero_index_stud = int(stud)-1
+                centers_answers = inquirer.prompt(centers)
+                cntrs = centers_answers['centers']
+                break
+            else:
+                raise ValueError
         except ValueError:
-            print(Fore.RED + Style.BRIGHT + 'Please only enter numbers')
+            print(Fore.RED + Style.BRIGHT + 'Please only enter numbers'+
+            ' shown on the list above')
+
+        
     while True:
         try:
             doors = float(input(Fore.BLACK + 'How many doors are in the wall?'
@@ -89,8 +82,8 @@ def get_stud_quantities(wall_length):
 
     studs = math.ceil(wall_length/cntrs)
     number_studs = studs + doors + corners
-    print(Fore.GREEN + f'You need {number_studs} studs' +
-                       'and {wall_length * 2}lm of tracks\n')
+    print(Fore.GREEN + Style.BRIGHT + f'You need {number_studs} studs' +
+                       f' and {wall_length * 2}lm of tracks\n')
 
 
 def get_insulations_quantites(wall_area):
@@ -100,9 +93,9 @@ def get_insulations_quantites(wall_area):
     print(Fore.GREEN + f'You have chosen {insulated}!')
 
     if insulated == "Yes":
-        print(Fore.GREEN + f'You need {wall_area} m2 of insulation\n')
+        print(Fore.GREEN + Style.BRIGHT + f'You need {wall_area} m2 of insulation\n')
     if insulated == "No":
-        print('no insulation')
+        print(Fore.GREEN + Style.BRIGHT + 'No insulation')
 
 
 def get_wall_linings_quantities(wall_area, wall_length):
@@ -112,16 +105,17 @@ def get_wall_linings_quantities(wall_area, wall_length):
     if lngs_answr == 'Yes':
         lining_quants = lining_options()
         total_linings_wall = lining_quants * wall_area
-        print(Fore.GREEN + f' You need {total_linings_wall}m2' +
-                           f' or {total_linings_wall/2.88}' +
-                           'total number of boards' +
+        print(Fore.GREEN + Style.BRIGHT + f' You need {total_linings_wall}m2' +
+                           f' or {math.ceil(total_linings_wall/2.88)}' +
+                           ' total number of boards' +
                            f' in total and {wall_area}m2 per layer' +
-                           ' or {wall_area/2.88} number of boards per layer')
-
-    print(Fore.BLACK +
+                           f' or {math.ceil(wall_area/2.88)} number of boards per layer')
+        print(Fore.BLACK +
           tabulate(lining_table, lining_headers, tablefmt='github\n'))
+
+   
     if lngs_answr == 'No':
-        print(Fore.GREEN + 'No wall linings needed')
+        print(Fore.GREEN + Style.BRIGHT + + 'No wall linings needed')
 
 
 def lining_options():
@@ -131,13 +125,13 @@ def lining_options():
                                    'How many linings would you like?' +
                                    '(Please choose upto 6 linings):')
             numberoflinings = int(lining_choices)
-            if numberoflinings > 1 and numberoflinings < 6:
+            if numberoflinings >= 1 and numberoflinings <= 6:
                 print(numberoflinings)
                 return numberoflinings
             else:
                 raise ValueError
         except ValueError:
-            print(Fore.RED + 'Sorry, please choose a number between 1 and 6')
+            print(Fore.RED + Style.BRIGHT + 'Sorry, please choose a number between 1 and 6')
 
 
 def main():
@@ -145,7 +139,14 @@ def main():
     Run all program functions
     """
     welcome()
-    wall_build_up()
+    
+    print(Fore.BLACK + 'Now let us enter your wall dimensions\n')
+    wall_area, wall_length = wall_meter_square_calc()
 
+    get_stud_quantities(wall_length)
+
+    get_insulations_quantites(wall_area)
+
+    get_wall_linings_quantities(wall_area, wall_length)
 
 main()
